798c659287704568fc82c6605c04a9dc92b1d0e4
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2021-09-08 10:25:18 -0400

    its dirty but it works rn

--- umm/src/main.rs
+++ umm/src/main.rs
@@ -1,9 +1,18 @@
 // check
 // run
 // test
-use clap::{App, Arg, SubCommand};
+// clean
+use clap::{App, SubCommand};
 use glob::glob;
 use std::{path::PathBuf, process::Command};
+
+fn root_dir() -> PathBuf {
+    PathBuf::from("../umm_test/")
+}
+
+fn build_dir() -> PathBuf {
+    root_dir().join("bin")
+}
 
 fn find(name: &str) -> String {
     let output = Command::new("which")
@@ -18,7 +27,9 @@ fn find(name: &str) -> String {
 
 fn find_jar_classpath() -> String {
     let mut jars = Vec::new();
-    let mut paths = glob("../umm_test/*.jar").expect("Failed to glob.");
+    let pattern = format!("{}/*.jar", root_dir().display());
+
+    let paths = glob(&pattern).expect("Failed to glob.");
     for entry in paths {
         match entry {
             Ok(path) => {
@@ -33,25 +44,101 @@ fn find_jar_classpath() -> String {
 
 fn compile(path: &PathBuf) {
     let javac_path = find("javac");
+
     let output = Command::new(javac_path)
         .arg("-cp")
         .arg(find_jar_classpath())
         .arg("-d")
-        .arg("../umm_test/bin/")
+        .arg(build_dir().as_path().to_str().unwrap())
         .arg("-sourcepath")
-        .arg("../umm_test/")
+        .arg(root_dir().as_path().to_str().unwrap())
         .arg("-Xlint:unchecked")
         .arg(path)
         .output()
         .expect(format!("Failed to compile {}.", path.display()).as_str());
 
-
-    let err = String::from_utf8(output.stderr).expect("Failed to parse output.");
-
-    let output = String::from_utf8(output.stdout).expect("Failed to parse output.");
+    let err = String::from_utf8(output.stderr).expect("Failed to parse stderr.");
+    let err = err.trim();
+    let output = String::from_utf8(output.stdout).expect("Failed to parse stdout.");
+    let output = output.trim();
 
     if err.len() > 0 {
         println!("{}", err);
+    } else {
+        println!(
+            "Note: There were no errors or warnings while compiling {}!",
+            path.display()
+        );
+    }
+
+    if output.len() > 0 {
+        println!("{}", output);
+    }
+}
+
+fn test(path: &PathBuf) {
+    let java_path = find("java");
+    let classpath = find_jar_classpath();
+    let classpath = format!("{}:{}", classpath, build_dir().as_path().to_str().unwrap());
+
+    let output = Command::new(java_path)
+        .arg("-cp")
+        .arg(classpath)
+        .arg("org.junit.runner.JUnitCore")
+        .arg(path.file_stem().unwrap().to_str().unwrap())
+        .output()
+        .expect(format!("Failed to compile {}.", path.display()).as_str());
+
+    let err = String::from_utf8(output.stderr).expect("Failed to parse stderr.");
+    let err = err.trim();
+    let output = String::from_utf8(output.stdout).expect("Failed to parse stdout.");
+    let output = output.trim();
+
+    if err.len() > 0 {
+        println!("{}", err);
+    }
+    // } else {
+    //     println!(
+    //         "Note: There were no errors or warnings while testing\t{}!",
+    //         path.display()
+    //     );
+    // }
+
+    if output.len() > 0 {
+        println!("--------------- OUTPUT ---------------");
+        println!("{}", output);
+    }
+}
+
+fn run(path: &PathBuf) {
+    let java_path = find("java");
+    let classpath = find_jar_classpath();
+    let classpath = format!("{}:{}", classpath, build_dir().as_path().to_str().unwrap());
+
+    let output = Command::new(java_path)
+        .arg("-cp")
+        .arg(classpath)
+        .arg(path.file_stem().unwrap().to_str().unwrap())
+        .output()
+        .expect(format!("Failed to compile {}.", path.display()).as_str());
+
+    let err = String::from_utf8(output.stderr).expect("Failed to parse stderr.");
+    let err = err.trim();
+    let output = String::from_utf8(output.stdout).expect("Failed to parse stdout.");
+    let output = output.trim();
+
+    if err.len() > 0 {
+        println!("{}", err);
+    } else {
+        println!(
+            "Note: There were no errors or warnings while running\t{}!",
+            path.display()
+        );
+    }
+
+    if output.len() > 0 {
+        println!("--------------- OUTPUT ---------------");
+        println!("{}", output);
     }
 }
 
@@ -64,47 +151,66 @@ fn main() {
             SubCommand::with_name("check")
                 .about("checks the java file for syntax errors")
                 .version("0.0.1")
-                .arg(
-                    Arg::with_name("FILE_NAME")
-                        .short("f")
-                        .help("name of the file to check"),
-                ),
+                .args_from_usage("<FILE_NAME>              'the Java file to check'"),
         )
         .subcommand(
             SubCommand::with_name("run")
                 .about("runs the java file and shows the output")
                 .version("0.0.1")
-                .arg(
-                    Arg::with_name("FILE_NAME")
-                        .short("f")
-                        .help("name of the file to check"),
-                ),
+                .args_from_usage("<FILE_NAME>              'the Java file to run'"),
         )
         .subcommand(
             SubCommand::with_name("test")
                 .about("runs the given junit test file")
                 .version("0.0.1")
-                .arg(
-                    Arg::with_name("FILE_NAME")
-                        .short("f")
-                        .help("name of the file to check"),
-                ),
+                .args_from_usage("<FILE_NAME>              'the Java file to test'"),
+        )
+        .subcommand(
+            SubCommand::with_name("clean")
+                .about("cleans all compiled classes")
+                .version("0.0.1"),
         )
         .get_matches();
 
     match matches.subcommand_name() {
         Some("check") => {
-            for entry in glob("../umm_test/*.java").expect("Failed to glob.") {
-                match entry {
-                    Ok(path) => {
-                        compile(&path);
-                    }
-                    Err(e) => println!("{:?}", e),
-                }  
-            }
+            let path = matches
+                .subcommand_matches("check")
+                .unwrap()
+                .value_of("FILE_NAME")
+                .unwrap();
+
+            compile(&root_dir().join(path));
         }
-        Some("run") => {}
-        Some("test") => {}
+        Some("run") => {
+            let path = matches
+                .subcommand_matches("run")
+                .unwrap()
+                .value_of("FILE_NAME")
+                .unwrap();
+            compile(&root_dir().join(path));
+            run(&root_dir().join(path));
+        }
+        Some("test") => {
+            let path = matches
+                .subcommand_matches("test")
+                .unwrap()
+                .value_of("FILE_NAME")
+                .unwrap();
+            compile(&root_dir().join(path));
+            test(&root_dir().join(path));
+        }
+        Some("clean") => {
+            std::fs::remove_dir_all(build_dir())
+                .expect(format!("Failed to remove {} directory.", build_dir().display()).as_str());
+            std::fs::create_dir_all(build_dir()).expect(
+                format!(
+                    "Failed to create {} directory after cleaning.",
+                    build_dir().display()
+                )
+                .as_str(),
+            );
+        }
         _ => {}
     };
 }
