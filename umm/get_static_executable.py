e027e480bfccb16c0e7b957a132749aa03d6363f
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2021-09-08 11:44:01 -0400

    push script

--- umm/get_static_executable.py
+++ umm/get_static_executable.py
@@ -1,5 +1,8 @@
 import os
 
-os.system("cargo clean")
-os.system("cargo build --release --target=x86_64-unknown-linux-musl")
-os.system("cp target/x86_64-unknown-linux-musl/release/umm ./umm")
+def do():
+    os.system("cargo clean")
+    os.system("cargo build --release --target=x86_64-unknown-linux-musl")
+    os.system("cp target/x86_64-unknown-linux-musl/release/umm ./umm")
+
+do()
---
+++ umm/push.py
@@ -0,0 +1,5 @@
+import os
+from get_static_executable import do
+
+do()
+os.system("git push origin main")
