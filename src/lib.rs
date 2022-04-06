3166d443a57c240e66af3107ff1f249d733a546e
dhruvdh <dhruv.dhamani@gmail.com>
2022-04-06 13:11:28 -0400

    todo list

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,16 @@ pub mod constants;
 pub mod util;
 pub mod java;
 
-// TODO Add file documentation
-// TODO fix JavaFile impl
-
 /// Defined for convenience
 type Dict = std::collections::HashMap<String, String>;
 
+
+// TODO: Add documentations everywhere
+// TODO: Fix java mod impls
+// TODO: remove fncmd
+// TODO: use reedline for shell-like interface
+// TODO: update classpath when discovering project
+// TODO: fix grading api, move to grade module.
+// TODO: add rhai scripting for grading
+// TODO: find a way to generate a rhai wrapper for all methods
+// TODO: add rhai scripting for project init
