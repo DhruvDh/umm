d93adcdc246ee3317a35fb17168043470e05f98d
dhruvdh <dhruv.dhamani@gmail.com>
2022-05-22 12:12:05 -0400

    clean up

--- build.fish
+++
@@ -1,8 +0,0 @@
-#!/bin/fish
-
-cross build --target x86_64-unknown-linux-gnu --release
-cp target/x86_64-unknown-linux-gnu/release/umm-check ./umm-check
-cp target/x86_64-unknown-linux-gnu/release/umm-run ./umm-run
-cp target/x86_64-unknown-linux-gnu/release/umm-clean ./umm-clean
-cp target/x86_64-unknown-linux-gnu/release/umm-test ./umm-test
-
