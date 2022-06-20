f5b7b655d3f57dc5f7c17df5d49262d58592e7ae
dhruvdh <dhruv.dhamani@gmail.com>
2022-06-20 10:45:35 -0400

    jar file url branch

--- src/java.rs
+++ src/java.rs
@@ -614,23 +614,23 @@ impl Project {
             }
 
             download(
-        "https://github.com/DhruvDh/umm/blob/next-assign1-spring-22/jar_files/junit-platform-console-standalone-1.8.0-RC1.jar?raw=true",
+        "https://github.com/DhruvDh/umm/blob/main/jar_files/junit-platform-console-standalone-1.8.0-RC1.jar?raw=true",
         &LIB_DIR.join("junit-platform-console-standalone-1.8.0-RC1.jar"),
 false    )?;
             download(
-        "https://github.com/DhruvDh/umm/blob/next-assign1-spring-22/jar_files/pitest-1.7.4.jar?raw=true",
+        "https://github.com/DhruvDh/umm/blob/main/jar_files/pitest-1.7.4.jar?raw=true",
         &LIB_DIR.join("pitest.jar"),
     false)?;
             download(
-        "https://github.com/DhruvDh/umm/blob/next-assign1-spring-22/jar_files/pitest-command-line-1.7.4.jar?raw=true",
+        "https://github.com/DhruvDh/umm/blob/main/jar_files/pitest-command-line-1.7.4.jar?raw=true",
         &LIB_DIR.join("pitest-command-line.jar"),
     false)?;
             download(
-        "https://github.com/DhruvDh/umm/blob/next-assign1-spring-22/jar_files/pitest-entry-1.7.4.jar?raw=true",
+        "https://github.com/DhruvDh/umm/blob/main/jar_files/pitest-entry-1.7.4.jar?raw=true",
         &LIB_DIR.join("pitest-entry.jar"),
     false)?;
             download(
-        "https://github.com/DhruvDh/umm/blob/next-assign1-spring-22/jar_files/pitest-junit5-plugin-0.14.jar?raw=true",
+        "https://github.com/DhruvDh/umm/blob/main/jar_files/pitest-junit5-plugin-0.14.jar?raw=true",
         &LIB_DIR.join("pitest-junit5-plugin.jar"),
    false )?;
         }
